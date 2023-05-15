c45122ea25fc2f4e21a9d658c33f71a45b4d905d
Mijin Sim <80371353+azure-553@users.noreply.github.com>
2023-05-15 10:58:05 +0900

    ✨ :: SearchBar 스타일 지정

---
+++ src/components/SearchBar/SearchBar.constant.js
@@ -0,0 +1,19 @@
+export const RECOMMENDATION_DELAY = 500;
+
+export const searchBarSizes = {
+  radius: 42,
+  inputPaddingX: 20,
+  inputPaddingY: 12,
+  icon: 21,
+};
+
+export const searchEnterKeyCode = {
+  ENTER: 13,
+  ARROW_UP: 38,
+  ARROW_DOWN: 40,
+};
+
+export const searchLength = {
+  INDEX_MAX: 8,
+  LIST_MAX: 7,
+};
