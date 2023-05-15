9d5a7b03cb20b7d3620de4980a686047800b4cff
Mijin Sim <80371353+azure-553@users.noreply.github.com>
2023-05-15 12:18:51 +0900

    ✨ :: useInput Custom hook 구현

---
+++ src/hooks/useInput.js
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+export const useInput = (initialValue) => {
+  const [value, setValue] = useState(initialValue);
+
+  const onChange = (e) => {
+    const {
+      target: { value },
+    } = e;
+
+    setValue(value);
+  };
+
+  return {value, onChange, setValue }
+};
